909f6a7214af6c727595f55345ec73ca549b27f9
KimSungJin <rainbow96bear@gmail.com>
2023-09-01 13:16:57 +0900

    위상정렬과 DFS 예시함수 추가 main은 DFS용

--- 1005/1005.go
+++ 1005/1005.go
@@ -20,21 +20,41 @@ func main(){
 		fmt.Fscan(rx, &buildNum, &ruleNum)
 		buildTime := make([]int,buildNum+1)
 		buildRule := make([][]int,buildNum+1)
-		inDegree := make([]int,buildNum+1)
 		for j := 1 ; j<=buildNum ; j++{
 			fmt.Fscan(rx,&buildTime[j])
 		}
-		for k:=0 ; k<ruleNum ; k++{
+		for k:=1 ; k<=ruleNum ; k++{
 			var key, value int
-			fmt.Fscan(rx, &key, &value)
+			fmt.Fscan(rx, &value, &key)
 			buildRule[key]=append(buildRule[key],value)
-			inDegree[value]++
 		}
 		fmt.Fscan(rx,&winBuild)
 
-		fmt.Fprintln(wx,TS(buildTime, buildRule, inDegree, winBuild))
+		fmt.Fprintln(wx,DFS(buildTime, buildRule, winBuild))
 	}
 }
+func DFS(buildTime []int, buildRule [][]int, winBuild int)int{
+	stack := make([]int,0)
+	stack = append(stack,winBuild)
+	check := make([]int,len(buildTime)+1)
+	answer := 0
+	copy(check,buildTime)
+	for len(stack) > 0 {
+		node := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		for _, neighbor := range buildRule[node]{
+			if check[neighbor] < check[node] + buildTime[neighbor]{
+				check[neighbor] = check[node] + buildTime[neighbor]
+			}
+			if answer < check[neighbor]{
+				answer = check[neighbor]
+			}
+			stack=append(stack,neighbor)
+		}
+	}
+	return answer
+}
+
 func TS(buildTime []int, buildRule [][]int, inDegree []int, winBuild int)int{
 	queue := make([]int,0)
 	for j:=0 ; j<len(buildTime) ;j++{
