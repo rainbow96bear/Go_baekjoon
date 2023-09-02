1368b4977d8392250e67a96773955cf18c7e6d80
KimSungJin <rainbow96bear@gmail.com>
2023-09-02 16:17:21 +0900

    10869번 사칙연산

---
+++ 10869/10869.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var a, b int
+	fmt.Fscan(reader, &a, &b)
+
+	fmt.Fprintln(writer,a+b)
+	fmt.Fprintln(writer,a-b)
+	fmt.Fprintln(writer,a*b)
+	fmt.Fprintln(writer,a/b)
+	fmt.Fprintln(writer,a%b)
+}
