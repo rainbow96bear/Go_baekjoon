816558b3a47519d28d69b88db7a240125eb7b75f
KimSungJin <rainbow96bear@gmail.com>
2023-09-04 00:41:05 +0900

    2439번 별 찍기 - 2

---
+++ 2439/2439.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var Num int
+	fmt.Fscan(reader, &Num)
+
+	for i:=1 ; i<=Num ; i++{
+		fmt.Fprint(writer,strings.Repeat(" ",Num-i))
+		fmt.Fprintln(writer,strings.Repeat("*",i))
+	}
+}
