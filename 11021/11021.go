62f7c02ad0602267bbb6e89ff402376ca3e9608e
KimSungJin <rainbow96bear@gmail.com>
2023-09-04 00:19:26 +0900

    11021번 A+B - 7

---
+++ 11021/11021.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var testNum int
+	fmt.Fscan(reader, &testNum)
+
+	for i:=1 ; i<=testNum ; i++{
+		var A, B int
+		fmt.Fscan(reader, &A, &B)
+		fmt.Fprintf(writer,"Case #%d: %d\n",i,A+B)
+	}
+}
