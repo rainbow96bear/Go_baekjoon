0bf0c35696e61410808c33ab54850890b87ff423
KimSungJin <rainbow96bear@gmail.com>
2023-09-07 21:54:06 +0900

    창문 닫기

---
+++ 13909/13909.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var N int
+	fmt.Fscan(reader, &N)
+
+	fmt.Fprint(writer, int(math.Pow(float64(N),0.5)))
+}
