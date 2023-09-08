d1ff0b411821e3710d581a719a405bd3e019fade
KimSungJin <rainbow96bear@gmail.com>
2023-09-08 22:26:28 +0900

    베라의 패션

---
+++ 15439/15439.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var N int
+	fmt.Fscan(reader, &N)
+
+	fmt.Fprint(writer, N*(N-1))
+}
