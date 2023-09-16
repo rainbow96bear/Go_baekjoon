2bb01671636e8ec61264d8ab2bc139a2251a0566
KimSungJin <rainbow96bear@gmail.com>
2023-09-16 15:57:17 +0900

    N 찍기

---
+++ 2741/2741.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var N int
+	fmt.Fscan(reader, &N)
+
+	for i:=1 ; i<=N ; i++ {
+		fmt.Fprintln(writer, i)
+	}
+}
