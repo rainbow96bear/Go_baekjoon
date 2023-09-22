24fe088b4c0afa99f0ad0e79c3a240bcfbab0468
KimSungJin <rainbow96bear@gmail.com>
2023-09-22 10:24:56 +0900

    2xn 타일링2

---
+++ 11727/11727.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	var n int
+	fmt.Fscan(reader, &n)
+
+	if n == 1 {
+		fmt.Fprintln(writer, 1)
+		return
+	}
+	dp := make([]int, n+1)
+    dp[1] = 1
+    dp[2] = 3
+
+    for i := 3; i <= n; i++ {
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007
+    }
+
+	fmt.Fprintln(writer, dp[n])
+}
