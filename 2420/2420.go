1127bce84137973e140f31f3946856a44e21486b
KimSungJin <rainbow96bear@gmail.com>
2023-12-30 21:18:26 +0900

    사파리월드

---
+++ 2420/2420.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	
+	defer writer.Flush()
+
+	var N, M int
+	fmt.Fscanf(reader, "%d %d\n", &N, &M)
+	result := N-M
+	if result < 0 {
+		result *= -1
+	}
+	fmt.Fprintln(writer, result)
+}
