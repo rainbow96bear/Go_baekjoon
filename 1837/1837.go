463db512d5d82282dc137dc206c083e6bfb90274
KimSungJin <rainbow96bear@gmail.com>
2024-01-27 12:53:32 +0900

    암호제작

---
+++ 1837/1837.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/big"
+	"os"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+	
+	var p, k string
+	fmt.Fscanf(reader , "%s %s\n", &p, &k)
+
+	pBigInt := new(big.Int)
+	kBigInt := new(big.Int)
+
+	pBigInt.SetString(p, 10)
+	kBigInt.SetString(k, 10)
+
+	smallestPrime := findSmallestPrime(pBigInt, kBigInt)
+
+	if smallestPrime != nil {
+		fmt.Println("BAD", smallestPrime)
+	} else {
+		fmt.Println("GOOD")
+	}
+}
+
+// 소수 체크 함수
+func isPrime(n *big.Int) bool {
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+
+	if n.Cmp(two) < 0 {
+		return false
+	}
+
+	for i := big.NewInt(2); i.Cmp(n) < 0; i.Add(i, one) {
+		if new(big.Int).Mod(n, i).Cmp(big.NewInt(0)) == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// 두 소수의 곱으로 이루어진 수에서 특정한 조건을 만족하는 가장 작은 소수 찾기
+func findSmallestPrime(p, k *big.Int) *big.Int {
+	one := big.NewInt(1)
+	zero := big.NewInt(0)
+
+	for i := big.NewInt(2); i.Cmp(k) < 0; i.Add(i, one) {
+			if new(big.Int).Mod(p, i).Cmp(zero) == 0 {
+				return i
+			}
+	}
+
+	return nil
+}
