7a762c5d458e58b239832542ef4b499f49e12d5d
KimSungJin <rainbow96bear@gmail.com>
2024-02-17 08:54:17 +0900

    세 수

---
+++ 10817/10817.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main(){
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	list := make([]int, 3)
+	fmt.Fscanf(reader, "%d %d %d\n", &list[0], &list[1], &list[2])
+
+	sort.Slice(list, func(i, j int)bool{return list[i] < list[j]})
+
+	fmt.Fprintln(writer, list[1])
+}
+
